02cc0d535938face24cc28848b5c5aa677839fcf
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2024-05-09 00:51:13 +0700

    rest-api

---
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,28 @@
+package com.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return service.findAll();
+    }
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable int id){
+        return service.findById(id);
+    }
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        service.save(user);
+    }
+}
