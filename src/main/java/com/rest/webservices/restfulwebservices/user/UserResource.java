78b0de141074ad9697edb57098890ebc8d3d2e6f
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2024-05-09 01:02:18 +0700

    location and status

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,15 +17,24 @@ public class UserResource {
     }
 
     @GetMapping("/users")
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return service.findAll();
     }
+
     @GetMapping("/users/{id}")
-    public User getUser(@PathVariable int id){
+    public User getUser(@PathVariable int id) {
         return service.findById(id);
     }
+
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
-        service.save(user);
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
