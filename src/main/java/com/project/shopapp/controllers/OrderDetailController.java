6cc3a8bbd198b7fbe26b183e81015d5deba20313
bacpham <bacph1996@gmail.com>
2024-08-24 00:31:02 +0700

    order detail

---
+++ src/main/java/com/project/shopapp/controllers/OrderDetailController.java
@@ -0,0 +1,36 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.dtos.OrderDetailDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/order_details")
+public class OrderDetailController {
+    @PostMapping("")
+    public ResponseEntity<?> createOrderDetail(@Valid @RequestBody OrderDetailDTO orderDetailDTO,
+                                               BindingResult result){
+        try {
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            return ResponseEntity.ok("Order detail created");
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/${id}")
+    public ResponseEntity<?> getOrderDetail(@Valid @PathVariable Long id){
+        return ResponseEntity.ok("Get order detail with id: " + id);
+    }
+}
---
+++ src/main/java/com/project/shopapp/dtos/OrderDetailDTO.java
@@ -0,0 +1,34 @@
+package com.project.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetailDTO {
+    @JsonProperty("order_id")
+    @Min(value = 1, message = "Order Id must be > 0")
+    private Long orderId;
+
+    @JsonProperty("product_id")
+    @Min(value = 1, message = "Product Id must be > 0")
+    private Long productId;
+
+    @Min(value = 0, message = "Price must be >= 0")
+    private Float price;
+
+    @Min(value = 1, message = "Quantity must be > 0")
+    private int quantity;
+
+    @JsonProperty("total_money")
+    @Min(value = 0, message = "Total money must be >= 0")
+    private Float totalMoney;
+
+    private String color;
+}
