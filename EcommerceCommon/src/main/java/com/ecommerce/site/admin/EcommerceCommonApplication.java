b74d271223a037b043330929bbb3b2502a8a9c59
bacphfx <bacph1996@gmail.com>
2024-09-11 22:32:40 +0700

    initialize project

---
+++ EcommerceCommon/src/main/java/com/ecommerce/site/admin/EcommerceCommonApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.site.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceCommonApplication.class, args);
+    }
+
+}
